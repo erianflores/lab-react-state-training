69a4ed6b2873bbe1df9e87c65b7c513d052e04fb
Erian Flores <erian.flores@gmail.com>
2025-04-13 14:07:22 +0200

    Dice added

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,5 +1,27 @@
 import { useState } from "react";
 
-const Dice = () => {
-    const [isClicked]
-};
+const diceImages = [
+  "../assets/images/dice-empty.png",
+  "../assets/images/dice1.png",
+  "../assets/images/dice2.png",
+  "../assets/images/dice3.png",
+  "../assets/images/dice4.png",
+  "../assets/images/dice5.png",
+  "../assets/images/dice6.png",
+];
+
+export default function Dice() {
+  const getRandomDice = () => diceImages[Math.floor(Math.random() * 6)];
+
+  const [dice, setDice] = useState(getRandomDice());
+
+  const handleClick = () => {
+    setDice("../assets/images/dice-empty.png");
+
+    setTimeout(() => {
+      setDice(getRandomDice());
+    }, 1000);
+  };
+
+  return <img src={dice} alt="dice" onClick={handleClick} />;
+}
