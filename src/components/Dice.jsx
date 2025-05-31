d0d3ca03d4690ffe3b02e32e32011644619af54a
Erian Flores <erian.flores@gmail.com>
2025-05-31 02:20:52 +0200

    updated iteration 4

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
+import diceEmpty from "../assets/images/dice-empty.png";
+import dice1 from "../assets/images/dice1.png";
+import dice2 from "../assets/images/dice2.png";
+import dice3 from "../assets/images/dice3.png";
+import dice4 from "../assets/images/dice4.png";
+import dice5 from "../assets/images/dice5.png";
+import dice6 from "../assets/images/dice6.png";
 
-const diceImages = [
-  "../assets/images/dice-empty.png",
-  "../assets/images/dice1.png",
-  "../assets/images/dice2.png",
-  "../assets/images/dice3.png",
-  "../assets/images/dice4.png",
-  "../assets/images/dice5.png",
-  "../assets/images/dice6.png",
-];
+const diceImages = [dice1, dice2, dice3, dice4, dice5, dice6];
 
 export default function Dice() {
   const getRandomDice = () => diceImages[Math.floor(Math.random() * 6)];
@@ -20,8 +19,8 @@ export default function Dice() {
 
     setTimeout(() => {
       setDice(getRandomDice());
-    }, 1000);
+    }, 0);
   };
 
-  return <img src={dice} alt="dice" onClick={handleClick} />;
+  return <img src={dice} alt="dice" className="dice" onClick={handleClick} />;
 }
